fa61da3db5e57f03e849f87ce5d589ab1ef9aa65
DESKTOP-Q7JC4TQ\Ariel <arielmoscarella35@gmail.com>
2022-06-19 16:00:35 +0200

    Algoritmo con CICLO

---
+++ EntregaClase2.js
@@ -0,0 +1,7 @@
+//PEDIR NUEMRO POR PROMPT Y SALIDA DE MENSAJE HOLA LA CANTIDAD DE VECES INGRESADA
+let numeroVeces = parseInt(prompt("Ingrese un numero:"));
+
+for(let i=1; i <= numeroVeces; i++){
+    console.log("Hola")
+};
+
