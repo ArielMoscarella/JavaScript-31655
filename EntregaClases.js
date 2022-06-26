a8f384a7868dfd97e824f1c19bb8f7e656022308
DESKTOP-Q7JC4TQ\Ariel <arielmoscarella35@gmail.com>
2022-06-26 11:16:57 +0200

    Simulador Interactivo

---
+++ EntregaClases.js
@@ -0,0 +1,58 @@
+//PEDIR NUEMRO POR PROMPT Y SALIDA DE MENSAJE HOLA LA CANTIDAD DE VECES INGRESADA
+
+//ENTREGA CLASE 2
+/*let numeroVeces = parseInt(prompt("Ingrese un numero:"));
+
+for(let i=1; i <= numeroVeces; i++){
+    console.log("Hola")
+};
+*/
+
+
+//ENTREGA CLASE 3
+let montoTotal= 0
+let valorCuotas= 0
+let monto= parseInt(prompt(message= "Ingrese el monto"))
+let cuotas= parseInt(prompt(message= "Numero de cuotas"))
+alert("Recuerda que tenes hasta 3 cuotas sin interes")
+
+
+switch(cuotas){
+    case 1:
+        console.log("Total a pagar" + "" + "$" + monto + "en 1 cuota" )
+    break;
+    case 2:
+        valorCuotas= monto/cuotas
+        console.log("Total a pagar" + "" + "$" + monto + "en 2 cuotas sin interes." + "Pagara" + "" + "$" + valorCuotas + "" +  "por cada una")
+    break;
+    case 3:
+        valorCuotas= monto/cuotas
+        console.log("Total a pagar" + "" + "$" + monto + "en 3 cuotas sin interes." + "Pagara" + "" + "$" + valorCuotas + "" + "por cada una")
+    break;
+    case 4:
+        valorCuotas= (monto/cuotas) * 1.055  
+        montoTotal= valorCuotas * cuotas
+        console.log("Total a pagar" + "" + "$" + monto + "en 4 cuotas." + "Pagara" + "" + "$" + valorCuotas + "" +   "por cada una." + "Dando un total de" + "" + "$" + montoTotal )
+    break;
+    case 6:
+        valorCuotas= (monto/cuotas) * 1.055  
+        montoTotal= valorCuotas * cuotas
+        console.log("Total a pagar" + "" + "$" + monto + "en 6 cuotas." + "Pagara" + "" + "$" + valorCuotas + "" +   "por cada una." + "Dando un total de" + "" + "$" + montoTotal )
+    break;
+    case 8:
+        valorCuotas= (monto/cuotas) * 1.055  
+        montoTotal= valorCuotas * cuotas
+        console.log("Total a pagar" + "" + "$" + monto + "en 8 cuotas." + "Pagara" + "" + "$" + valorCuotas + "" +   "por cada una." + "Dando un total de" + "" + "$" + montoTotal )
+    break;
+    case 10:
+        valorCuotas= (monto/cuotas) * 1.055  
+        montoTotal= valorCuotas * cuotas
+        console.log("Total a pagar" + "" + "$" + monto + "en 10 cuotas." + "Pagara" + "" + "$" + valorCuotas + "" +   "por cada una." + "Dando un total de" + "" + "$" + montoTotal ) 
+    break;
+    case 12:
+        valorCuotas= (monto/cuotas) * 1.055  
+        montoTotal= valorCuotas * cuotas
+        console.log("Total a pagar" + "" + "$" + monto + "en 12 cuotas." + "Pagara" + "" + "$" + valorCuotas + "" +   "por cada una." + "Dando un total de" + "" + "$" + montoTotal )
+    break;
+}
+
