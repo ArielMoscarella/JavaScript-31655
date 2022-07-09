d5a6a0ab2f27364e614d92d80be591b23f17caaa
DESKTOP-Q7JC4TQ\Ariel <arielmoscarella35@gmail.com>
2022-07-09 10:03:40 +0200

    Arrays y Primera Entrega de Proyecto Final

--- EntregaClases.js
+++ EntregaClases.js
@@ -10,7 +10,7 @@ for(let i=1; i <= numeroVeces; i++){
 
 
 //ENTREGA CLASE 3
-let montoTotal= 0
+/*let montoTotal= 0
 let valorCuotas= 0
 let monto= parseInt(prompt(message= "Ingrese el monto"))
 let cuotas= parseInt(prompt(message= "Numero de cuotas"))
@@ -55,4 +55,72 @@ switch(cuotas){
         console.log("Total a pagar" + "" + "$" + monto + "en 12 cuotas." + "Pagara" + "" + "$" + valorCuotas + "" +   "por cada una." + "Dando un total de" + "" + "$" + montoTotal )
     break;
 }
+*/
+
+//ENTREGA CLASE 4.1 y 4.2
+let cuotas = {1:0 , 2:0 , 3:0 , 4:0 ,5:0 ,6:0 ,7:0 ,8:0 ,9:0 ,10:0 ,11:0 ,12:0 }
+let numeroCuotas = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+
+nombreusuario= prompt("Ingrese nombre")     
+apellidousuario= prompt("Ingrese apellido")
+precioparcial= prompt("Importe parcial de la compra (Sin IVA)")
+let cuotasUsuario = prompt("Ingrese en cuantas cuotas quiere abonar") 
+
+
+class Compra {
+
+   
+
+    constructor(nombre,apellido,precio){
+        this.nombre = nombre;               
+        this.apellido = apellido;           
+        this.precio = precio;
+    }
+
+    usuario(){
+
+        return `${this.nombre} ${this.apellido}` 
+    
+    };
+
+    sumaIva(){
+       return this.precio = this.precio * 1.21;     
+    };
+
+    valorCuotas() {
+        let valorXCuota= 0
+
+        valorXCuota = this.precio / cuotasUsuario;
+        return console.log(valorXCuota);
+
+       
+    };
+
+    imprimirCuotas() {
+        
+        for(let i = 0 ; i < numeroCuotas ; i++){
+            return console.log(cuotas[i])
+        };
+    
+    };
+ 
+
+}
+
+const persona= new Compra (nombreusuario, apellidousuario, precioparcial);
+
+const precioFinal = persona.sumaIva()
+
+const nombreCompleto = persona.usuario()
+
+persona.valorCuotas();
+
+persona.imprimirCuotas();
+
+console.log(persona)
+
+
+alert(nombreCompleto + ", su compra de " + precioFinal +".Fue realizada con exito." )
+
 
